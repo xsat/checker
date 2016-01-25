43658e04246c422a4b097a3a8fb0577455baabd7
VSITS-3 <C:\Users\VSITS-3\AppData\Roaming\The Bat!>
2016-01-25 10:38:23 +0200

    version 2

--- checker.pro
+++ checker.pro
@@ -1,11 +1,17 @@
-QT += core
-QT -= gui
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-01-25T10:09:13
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 TARGET = checker
-CONFIG += console
-CONFIG -= app_bundle
-
 TEMPLATE = app
+
 
 SOURCES += main.cpp
 
+HEADERS  +=
--- main.cpp
+++ main.cpp
@@ -1,48 +1,42 @@
+#include <QString>
 #include <QCoreApplication>
 #include <QProcess>
-#include <QString>
-#include <QTextStream>
 #include <QFile>
 #include <QDateTime>
+#include <QTimer>
 
+void createReport(QString &report);
 bool ping(const QString &address);
+void writeReport(const QString &report);
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    QString report;
-    QTextStream out(stdout);
-
-    out << "Start [" << endl;
-
-    for (int i = 0; i < 2; i++) {
-        QString address("10.246.4." + QString::number(i));
-
-        if (ping(address)) {
-            report += address + ";\n";
-        }
-
-        out << "    " << address << endl;
-    }
-
-    QFile file("reports/" + QDate().currentDate().toString(Qt::ISODate) + ".txt");
-
-    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        file.write(report.toUtf8());
-        file.close();
-    }
-
-    out << "] End" << endl;
+    QString report("");
+    createReport(report);
+    writeReport(report);
+    QTimer::singleShot(0, &a, SLOT(quit()));
 
     return a.exec();
+}
+
+void createReport(QString &report)
+{
+    for (int i = 0; i <= 255; i++) {
+        QString address("10.246.4." + QString::number(i));
+
+        if (!ping(address)) {
+            report += address + ";\n";
+        } else {
+            report += "\n";
+        }
+    }
 }
 
 bool ping(const QString &address)
 {
     QProcess ping;
     ping.start("ping", QStringList() << address);
-
     QString line("");
 
     if (ping.waitForFinished() ) {
@@ -52,4 +46,14 @@ bool ping(const QString &address)
     }
 
     return line.contains("TTL");
+}
+
+void writeReport(const QString &report)
+{
+    QFile file(QDate().currentDate().toString(Qt::ISODate) + ".txt");
+
+    if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        file.write(report.toUtf8());
+        file.close();
+    }
 }
